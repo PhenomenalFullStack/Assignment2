7116bd2e00cfbdcef207c1abc46bdf3515ec8f31
Phenomenal <jabezmomentum@gmail.com>
2023-03-11 20:15:06 +0200

    AllFilesCreated

---
+++ Assignment2-CS/Actinopterygii.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2_CS
+{
+    class Actinopterygii : Chordata
+    {
+        public Actinopterygii()
+        {
+
+        }
+    }
+}
